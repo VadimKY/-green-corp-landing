937345d39e02a2040693cbd27750b1b2f89f953b
Vadim Kolerov <VYKolerov@mail.ru>
2022-12-03 11:35:50 +0300

    Анимация пузырьков js

--- new.js
+++ new.js
@@ -1,1 +1,64 @@
-console.log(String([1, true, [], {}]))
+/*let goods = {
+  title: "Самокат", 
+  price: 6000,
+  good_id: 1
+};
+let val;
+let goodsArray = Object.keys(goods);
+let goodsArray_2 = Object.values(goods);
+   for (let i = 0; i < goodsArray_2.length; i++) {
+   val = goodsArray_2[i];
+   goodsArray.push(val);
+};
+
+console.log(goodsArray); //['title', 'price', 'good_id', 'Самокат', 6000, 1]*/
+
+/*let article = {
+  title: "Загадки дачного огурца", 
+  text: "Огурец не перестает удивлять своими выходками. Хозяйка в недоумении" 
+};
+author = {
+  name: "Ричард М.В.",
+  age: 43
+};
+let clone = Object.assign(article, author);
+console.log(clone); //{title: 'Загадки дачного огурца', text: 'Огурец не перестает удивлять своими выходками. Хозяйка в недоумении', 
+name: 'Ричард М.В.', 
+age: 43}
+*/
+/*
+
+
+let array = []
+function count() {
+    let num = array.length;
+    console.log("Количество элементов в массиве:", num);
+};
+count();*/
+
+
+/*let array = []
+function count() {
+  let counter = 0;
+  for (let el of array) {
+      counter ++;
+  }
+  return counter;
+}
+let rez = count();
+console.log("Количество элементов в массиве:",rez);
+*/
+/*
+let array = []
+console.log(array.size)
+
+function count() {
+    console.log("Количество элементов в массиве:", array.size);
+};
+count();*/
+ctx.beginPath();
+ctx.moveTo(100, 100);
+ctx.lineTo(200, 100);
+ctx.beginPath();
+ctx.moveTo(100, 150);
+ctx.lineTo(200, 150);
